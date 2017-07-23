75d133018cbdca45a8f189c607d7802832da43e4
iwakawa <df533738@db3.so-net.ne.jp>
2017-07-23 15:53:22 +0900

    follow

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -13,7 +13,7 @@ class ConversationsController < ApplicationController
       @conversation = Conversation.create!(conversation_params)
     end
 
-    redirect_to conversation_messages_path(@conversation)
+    redirect_to :action => "index"
   end
 
   private
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -28,7 +28,7 @@ class MessagesController < ApplicationController
   def create
     @message = @conversation.messages.build(message_params)
     if @message.save
-     redirect_to conversation_messages_path(@conversation)
+     redirect_to :action => "messages#index"
     end
   end
 
